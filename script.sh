e674ec7a8d68dbc4b81c65fafb0561cc14b83b34
Tomás Bustamante Schwember <tomybustamante@gmail.com>
2022-03-15 15:11:16 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,3 +5,19 @@ echo ' _       __         ______             __
 | |/ |/ //  __/    / /  /  __// /__ / / / /(__  ) 
 |__/|__/ \___/    /_/   \___/ \___//_/ /_//____/  
 ' > /etc/motd
+rm /etc/profile.d/sshpwd.sh > /dev/null 2>&1
+echo '#!/bin/bash
+let actividad="$(/usr/bin/cut -d. -f1 /proc/uptime)"
+let seg=$((${actividad}%60))
+let mins=$((${actividad}/60%60))
+let horas=$((${actividad}/3600%24))
+let dias=$((${actividad}/86400))
+uptime=`printf "%d dias, %02dh%02dm%02ds" "$dias" "$horas" "$mins" "$seg"`
+echo Uptime: $uptime
+
+weid=$(cat /root/modbus/weid.txt)
+echo We ID: $weid
+
+serial=$(cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2)
+echo Serial: $serial
+' > /etc/update-motd.d/10-uname
